0509d72ff16c8c4c18abad91dc5277caaf78b971
GaMaki00 <108995478+GaMaki00@users.noreply.github.com>
2026-03-09 22:16:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ if excel_file and pdf_file:
                                 pdf_data.append({
                                     'ID': student_id,
                                     'คะแนน_PDF': nums[-3] if len(nums) >= 3 else nums[-2],
-                                    'เกรด_PDF': nums[-2] if len(nums) >= 2 else nums[-1]
+                                    'เกรด_PDF': nums[-1] if len(nums) >= 1 else nums[-1]
                                 })
             df_pdf_all = pd.DataFrame(pdf_data).drop_duplicates(subset=['ID'])
 
